50b1dde9abad33e5260e503833e79a0abf6e9002
PaulAdamovich <117354046+PaulAdamovich@users.noreply.github.com>
2023-02-25 17:12:30 +0400

    first commit

---
+++ src/main/java/org/example/comparable/Test1.java
@@ -0,0 +1,21 @@
+package org.example.comparate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Ivan");
+        names.add("Maria");
+        names.add("Petr");
+        names.add("Andrey");
+        names.add("Zlatan");
+        System.out.println("Before sorting");
+        System.out.println(names);
+        Collections.sort(names);
+        System.out.println("After sorting");
+        System.out.println(names);
+    }
+}
---
+++ src/main/java/org/example/comparable/Test2.java
@@ -0,0 +1,60 @@
+package org.example.comparate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Test2 {
+    public static void main(String[] args) {
+        List<Employee> employeeList = new ArrayList<>();
+        Employee emp1 = new Employee(23, "Ivan", "Abramov", 780);
+        Employee emp2 = new Employee(45, "Petr", "Ivanov", 456);
+        Employee emp3 = new Employee(12, "Pavel", "Dudorev", 900);
+        Employee emp4 = new Employee(87, "Stas", "Vladov", 300);
+        employeeList.add(emp1);
+        employeeList.add(emp2);
+        employeeList.add(emp3);
+        employeeList.add(emp4);
+        System.out.println("Before sorting \n " + employeeList);
+        Collections.sort(employeeList);
+        System.out.println("After sorting \n " + employeeList);
+    }
+
+}
+
+class Employee implements Comparable<Employee>{
+    int id;
+    String name;
+    String surname;
+    int salary;
+
+    public Employee(int id, String name, String surname, int salary) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Employee anotherEmp) {
+        if (this.id == anotherEmp.id){
+            return 0;
+        }
+        else if (this.id < anotherEmp.id){
+            return -1;
+        }
+        else {
+            return 1;
+        }
+    }
+}
